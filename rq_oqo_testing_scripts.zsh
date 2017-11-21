1c91921745aa299a12cd76106987c79946d5af84
indieterminacy <indieterminacy@nortonspeal.com>
2017-11-21 15:08:14 +0100

    Added seperate BDD tests from original hq_nqn_navigating_pathways script

---
+++ rq_oqo_testing_scripts.zsh
@@ -0,0 +1,25 @@
+#!/usr/bin/zsh
+
+# GIVEN Request made to move to folder
+# WHEN No argument given
+# THEN Existing path given
+
+# GIVEN Request made to move to folder
+# WHEN Argument '0' given
+# THEN Home page path given
+
+# GIVEN Request made to move to folder
+# WHEN Argument '-' given
+# THEN Parent page path given
+
+# GIVEN Request made to move to folder
+# WHEN Argument '=' given
+# THEN Folder of Git project is given
+
+# GIVEN Request made to move to folder
+# WHEN Argument 'oq' given
+# THEN Folder for program, scripts or tool is given
+
+# GIVEN Request made to move to folder
+# WHEN Argument 'rqr' given
+# THEN Directory is created if the pathway does not exist
